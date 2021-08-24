dfc1d79542b1fdef6739709ced92e869602063b7
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-24 10:35:28 +0300

    db server

---
+++ src/db.js
@@ -0,0 +1,70 @@
+import mysql from "mysql";
+
+const connectOptions = { // prisijungimas prie duomenu bazes
+    host: "localhost",
+    user: "nodejs",
+    password: "nodejs123456",
+    database: "zmones",
+    multipleStatements: true,
+};
+
+function dbConnect() {
+    const conn = mysql.createConnection(connectOptions);
+    return new Promise((resolve, reject) => {
+        conn.connect((err) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve(conn);
+        });
+    });
+}
+
+function dbDisconnect(conn) {
+    if (conn) {
+        return new Promise((resolve, reject) => {
+            conn.end((err) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve();
+            });
+        });
+    } else {
+        return Promise.resolve();
+    }
+}
+
+function dbQuery(conn, ...args) {
+    return new Promise((resolve, reject) => {
+        conn.query(...args, (err, results, fields) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve({
+                results,
+                fields,
+            });
+        });
+    });
+}
+
+async function getZmones() {
+    let conn;
+    try {
+        conn = await dbConnect();
+        let r = await dbQuery(conn, "select id, vardas, pavarde, gim_data as gimimoData, alga from zmones"); // duomenu paemimas is duomenu bazes
+        console.log(r.results);
+        return r.results;
+    } finally {
+        try {
+            await dbDisconnect(conn);
+        } catch (err) {
+        }
+    }
+}
+
+export { getZmones };
--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import { default as express } from "express";
 import exphbs from "express-handlebars";
 
+import { getZmones } from "./db.js";
+
 const app = express(); // paleidziama funkcija is node_modules
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
@@ -13,13 +15,10 @@ app.use(express.json()); // ateina duomenys json
 app.get("/", async (req, res) => {
     res.type("text/html");
     try {
-        const fZmones = await readFile("zmones.json", {
-            encoding: "utf8",
-        });
-        const zmones = JSON.parse(fZmones);
-        res.render("zmones", { zmones });
-    }
-    catch (err) {
+        const zmones = getZmones(); // gaunamas sarasas
+        res.render("zmones", { zmones }); // nusiunciamas sarasas i narsykle
+    } catch (err) {
+        console.log(err);
         res.status(500).send(err);
     }
 });
