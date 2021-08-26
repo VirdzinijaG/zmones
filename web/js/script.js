b72a227d2fe4bb28cd3b8b243af438db7b1cf352
Virdžinija <virdzinija.cikanaviciute@gmail.com>
2021-08-26 12:07:18 +0300

    function getZmones, create table

--- web/js/script.js
+++ web/js/script.js
@@ -4,12 +4,35 @@ async function getZmones() {
         const res = await fetch("/json/zmones") // gaunami duomenys is /json/zmones ir grazina promisa
         if (res.ok) {
             const data = await res.json();
-            console.log(data); // console atspausdinama sarasas duomenu
+            // console.log(data); // console atspausdinama sarasas duomenu
+            const div = document.getElementById("zmones"); // ieskomas elementas html pagal id
+            const table = document.createElement("table"); // elemento sukurimas html
+            for (const zmogus of data) {
+                const tr = document.createElement("tr");
+                let td;
+                td = document.createElement("td");
+                td.appendChild(document.createTextNode(zmogus.id)); // append elemento pridejimas // createTextNode sukuriamas tekstas
+                tr.appendChild(td);
+                td = document.createElement("td");
+                td.appendChild(document.createTextNode(zmogus.vardas));
+                tr.appendChild(td);
+                td = document.createElement("td");
+                td.appendChild(document.createTextNode(zmogus.pavarde));
+                tr.appendChild(td);
+                td = document.createElement("td");
+                td.appendChild(document.createTextNode(zmogus.gimimoData));
+                tr.appendChild(td);
+                td = document.createElement("td");
+                td.appendChild(document.createTextNode(zmogus.alga));
+                tr.appendChild(td);
+                table.appendChild(tr);
+            }
+            div.appendChild(table); // i div'a ikeliama lentele
         } else {
             console.log("Uzklausa is serverio atejo su klaida", res.status);
         }
     }
     catch (err) {
-        console.log("Kalida gaunant duomenis is serverio", err);
+        console.log("Klaida gaunant duomenis is serverio", err);
     }
 }
